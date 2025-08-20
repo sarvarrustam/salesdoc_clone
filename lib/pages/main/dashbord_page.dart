826d3222ee6486781f7cee5cf8c46d8a8a4f6833
Sarvar Rustam <sarvarrustam12@gmail.com>
2025-08-20 09:56:49 +0500

    first commit

---
+++ lib/pages/main/dashbord_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DashbordPage extends StatefulWidget {
+  const DashbordPage({super.key});
+
+  @override
+  State<DashbordPage> createState() => _DashbordPageState();
+}
+
+class _DashbordPageState extends State<DashbordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          height: 50,
+          color: Colors.green,
+          child: Row(
+            children: [
+              FlutterLogo(size: 50),
+              const SizedBox(width: 20),
+              ButtonWidget(text: ('Supervayzer')),
+              ButtonWidget(text: ('Savdo')),
+              ButtonWidget(text: ('Foinansiy')),
+              ButtonWidget(text: ('Foinansiy')),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  const ButtonWidget({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
+  }
+}
